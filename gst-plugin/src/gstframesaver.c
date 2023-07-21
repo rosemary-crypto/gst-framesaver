8a4a9aca9292fe134a5a36207d889ec0c2c1eba7
rosemary-crypto <rosekoikara@gmail.com>
2023-07-21 16:59:39 +0900

    Mux but gint error

--- gst-plugin/src/gstframesaver.c
+++ gst-plugin/src/gstframesaver.c
@@ -1,8 +1,6 @@
 #ifdef HAVE_CONFIG_H
-#  include <config.h>
+#include <config.h>
 #endif
-
-#include "gstframesaver.h"
 
 #include "gstframesaver.h"
 
@@ -20,21 +18,18 @@ GST_DEBUG_CATEGORY_STATIC (gst_frame_saver_debug);
 G_DEFINE_TYPE (GstFrameSaver, gst_frame_saver, GST_TYPE_ELEMENT);
 
 static GstStaticPadTemplate sink_template = GST_STATIC_PAD_TEMPLATE(
-    "sink", GST_PAD_SINK, GST_PAD_ALWAYS,
+    "sink", GST_PAD_SINK, GST_PAD_REQUEST,
     GST_STATIC_CAPS("video/x-raw, format=RGB, width=" GST_VIDEO_SIZE_RANGE ", height=" GST_VIDEO_SIZE_RANGE ", framerate=" GST_VIDEO_FPS_RANGE)
 );
 
 static GstStaticPadTemplate src_template = GST_STATIC_PAD_TEMPLATE
     ("src", GST_PAD_SRC, GST_PAD_ALWAYS, GST_STATIC_CAPS("video/x-raw"));
 
-static void 
-gst_frame_saver_finalize(GObject *object)
-{
-  GstFrameSaver *saver = GST_FRAME_SAVER(object);
+static void gst_frame_saver_finalize(GObject *object) {
+    GstFrameSaver *saver = GST_FRAME_SAVER(object);
+    GST_DEBUG_OBJECT(saver, "Finalizing");
 
-  GST_DEBUG_OBJECT(saver, "Finalizing");
-
-  G_OBJECT_CLASS(parent_class)->finalize(object);
+    G_OBJECT_CLASS(gst_frame_saver_parent_class)->finalize(object);
 }
 
 static GstFlowReturn gst_frame_saver_chain(GstPad *pad, GstObject *parent, GstBuffer *buffer) {
@@ -64,69 +59,79 @@ static GstFlowReturn gst_frame_saver_chain(GstPad *pad, GstObject *parent, GstBu
     gst_pad_set_caps(saver->srcpad, src_caps);
     gst_caps_unref(src_caps);
 
-    // Convert the frame to grayscale (assuming it's in RGB format)
-    GstMapInfo map;
-    gst_buffer_map(buffer, &map, GST_MAP_WRITE);
-    guint8 *data = map.data;
-    for (gsize i = 0; i < map.size; i += 3) {
-        guint8 gray = (data[i] + data[i + 1] + data[i + 2]) / 3;
-        data[i] = data[i + 1] = data[i + 2] = gray;
-    }
-    gst_buffer_unmap(buffer, &map);
-
-    // Continue processing the buffer and push it downstream
+    // Forward the buffer to the single queue for downstream processing
     return gst_pad_push(saver->srcpad, buffer);
 }
 
-
+// ... (rest of the code remains the same as before)
 
 static void gst_frame_saver_init(GstFrameSaver *saver) {
-    saver->sinkpad = gst_pad_new_from_static_template(&sink_template, "sink");
-    gst_pad_set_chain_function(saver->sinkpad, GST_DEBUG_FUNCPTR(gst_frame_saver_chain));
-    gst_element_add_pad(GST_ELEMENT(saver), saver->sinkpad);
-    saver->srcpad = gst_pad_new_from_static_template(&src_template, "src");
-    gst_element_add_pad(GST_ELEMENT(saver), saver->srcpad);
-    saver->frameCount = 0;
+    GstElementClass *element_class = GST_ELEMENT_CLASS(g_type_class_peek(GST_TYPE_FRAME_SAVER));
+
+    // Replace the existing element class with GstBinClass
+    g_type_class_adjust_private_offset(element_class, (gint)sizeof(GstBinClass));
+
+    // Create a bin instead of a simple element
+    saver->bin = gst_bin_new("framesaver_bin");
+    gst_element_set_state(GST_ELEMENT(saver->bin), GST_STATE_NULL);
+
+    // Add the bin to the element
+    gst_bin_add(GST_BIN(saver), saver->bin);
+
+    // Create a single queue for buffering processed frames
+    saver->queue = gst_element_factory_make("queue", "output_queue");
+    gst_bin_add(GST_BIN(saver->bin), saver->queue);
+
+    // Create tee element for parallel processing
+    saver->tee = gst_element_factory_make("tee", "tee");
+    gst_bin_add(GST_BIN(saver->bin), saver->tee);
+
+    // Link elements: tee -> queue -> sinkpad
+    gst_element_link_many(saver->tee, saver->queue, saver->sinkpad, NULL);
+
+    // Link tee pads to the queue
+    GstPad *tee_pad = gst_element_get_request_pad(saver->tee, "src_%u");
+    GstPad *queue_pad = gst_element_get_static_pad(saver->queue, "sink");
+    gst_pad_link(tee_pad, queue_pad);
+    gst_object_unref(tee_pad);
+    gst_object_unref(queue_pad);
 }
 
+// ... (rest of the code remains the same as before)
 
-static void
-gst_frame_saver_class_init (GstFrameSaverClass * klass)
+
+
+
+
+static void gst_frame_saver_class_init (GstFrameSaverClass * klass)
 {
-  GObjectClass *gobject_class;
-  GstElementClass *gstelement_class;
+    GObjectClass *gobject_class;
+    GstElementClass *gstelement_class;
 
-  gobject_class = G_OBJECT_CLASS(klass);
-  gstelement_class = GST_ELEMENT_CLASS(klass);
-  
-  gobject_class->finalize = gst_frame_saver_finalize;
+    gobject_class = G_OBJECT_CLASS(klass);
+    gstelement_class = GST_ELEMENT_CLASS(klass);
+    
+    gobject_class->finalize = gst_frame_saver_finalize;
 
-  // Set the pad templates to allow video input and output
-  gst_element_class_add_pad_template(gstelement_class,
-      gst_static_pad_template_get(&sink_template));
-  gst_element_class_add_pad_template(gstelement_class,
-      gst_static_pad_template_get(&src_template));
-
-  gst_element_class_set_details_simple (gstelement_class,
-      "FrameSaver",
-      "FIXME:Generic",
-      "FIXME:Generic Template Element", "U-DESKTOP-I8GIKDLPINTEL <<user@hostname.org>>");
-
+    // Set the pad templates to allow video input and output
+    gst_element_class_add_pad_template(gstelement_class,
+        gst_static_pad_template_get(&sink_template));
+    gst_element_class_add_pad_template(gstelement_class,
+        gst_static_pad_template_get(&src_template));
+    gst_element_class_set_details_simple (gstelement_class,
+        "FrameSaver",
+        "FIXME:Generic",
+        "FIXME:Generic Template Element", "U-DESKTOP-I8GIKDLPINTEL <<user@hostname.org>>");
 }
-
-
 
 /* entry point to initialize the plug-in
  * initialize the plug-in itself
  * register the element factories and other features
  */
-static gboolean
-plugin_init (GstPlugin * plugin)
-{
-  GST_DEBUG_CATEGORY_INIT(gst_frame_saver_debug, "framesaver", 0, "Frame Saver");
-  return gst_element_register (plugin, "framesaver", GST_RANK_NONE, GST_TYPE_FRAME_SAVER);
+static gboolean plugin_init(GstPlugin *plugin) {
+    GST_DEBUG_CATEGORY_INIT(gst_frame_saver_debug, "framesaver", 0, "Frame Saver");
+    return gst_element_register(plugin, "framesaver", GST_RANK_NONE, GST_TYPE_FRAME_SAVER);
 }
-
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
@@ -134,4 +139,4 @@ GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     "Frame Saver Plugin",
     plugin_init,
     PACKAGE_VERSION, GST_LICENSE, GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN
-    )
+)
--- gst-plugin/src/gstframesaver.h
+++ gst-plugin/src/gstframesaver.h
@@ -3,26 +3,30 @@
 
 #include <gst/gst.h>
 
-
 G_BEGIN_DECLS
 
 #define GST_TYPE_FRAME_SAVER (gst_frame_saver_get_type())
-G_DECLARE_FINAL_TYPE (GstFrameSaver, gst_frame_saver,
-    GST, FRAME_SAVER, GstElement)
+G_DECLARE_FINAL_TYPE (GstFrameSaver, gst_frame_saver, GST, FRAME_SAVER, GstElement)
+
+#define MAX_INPUT_STREAMS 4 // Just considering this as the max number of input streams for now
 
 struct _GstFrameSaver
 {
-  GstElement     element;
+    GstElement element;
 
-  GstPad        *sinkpad;
-  GstPad        *srcpad;
+    GstPad *sinkpad;
+    GstPad *srcpad;
 
-  guint frameCount;
+    guint frameCount;
 
+    GstElement *tee;
+    GstElement *queue;
+
+    GstBin *bin;
 };
 
 struct _GstFrameSaverClass {
-  GstElementClass parent_class;
+    GstElementClass parent_class;
 };
 
 
